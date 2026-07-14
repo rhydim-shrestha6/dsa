ad82a9460bfceef7c5ecc39c325d874b02baf3f6
rhydim-shrestha6 <rhydim6@gmail.com>
2026-07-14 06:53:22 +0545

    Add files via upload

--- infix_to_prefix.cpp
+++ infix_to_prefix.cpp
@@ -1,10 +1,11 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int prec(char c) {
+int prec(char c)
+{
     if (c == '^')
         return 3;
-    else if (c == '/' || c == '*')
+    else if (c == '*' || c == '/')
         return 2;
     else if (c == '+' || c == '-')
         return 1;
@@ -12,47 +13,83 @@ int prec(char c) {
         return -1;
 }
 
-void infixToPostfix(string s) {
+string infixToPostfix(string s)
+{
     stack<char> st;
     string result;
 
-    for (int i = 0; i < s.length(); i++) {
+    for (int i = 0; i < s.length(); i++)
+    {
         char c = s[i];
 
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+        if (isalnum(c))
             result += c;
 
         else if (c == '(')
-            st.push('(');
+            st.push(c);
 
-        else if (c == ')') {
-            while (st.top() != '(') {
+        else if (c == ')')
+        {
+            while (!st.empty() && st.top() != '(')
+            {
                 result += st.top();
                 st.pop();
             }
             st.pop();
         }
 
-        else {
-            while (!st.empty() && prec(s[i]) <= prec(st.top())) {
+        else
+        {
+            while (!st.empty() && prec(c) < prec(st.top()))
+            {
                 result += st.top();
                 st.pop();
             }
+
+            while (!st.empty() && prec(c) == prec(st.top()) && c != '^')
+            {
+                result += st.top();
+                st.pop();
+            }
+
             st.push(c);
         }
     }
 
-    while (!st.empty()) {
+    while (!st.empty())
+    {
         result += st.top();
         st.pop();
     }
 
-    cout << "Postfix expression: " << result << endl;
+    return result;
 }
 
-int main() {
+string infixToPrefix(string s)
+{
+    reverse(s.begin(), s.end());
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(')
+            s[i] = ')';
+        else if (s[i] == ')')
+            s[i] = '(';
+    }
+
+    string postfix = infixToPostfix(s);
+
+    reverse(postfix.begin(), postfix.end());
+
+    return postfix;
+}
+
+int main()
+{
     string exp = "(p+q)*(m-n)";
-    cout << "Infix expression: " << exp << endl;
-    infixToPostfix(exp);
+
+    cout << "Infix  : " << exp << endl;
+    cout << "Prefix : " << infixToPrefix(exp);
+
     return 0;
 }
